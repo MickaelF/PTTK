41a90448cfbd349826f7a53459238d2d2e57cae9
Mickaël Fleurus <fleurusmickael@gmail.com>
2020-07-12 11:59:28 +0200

    Correct dumb mistake in file inclusion for log parser projects.

---
+++ extra/logParser/parserSrcs.cmake
@@ -0,0 +1,6 @@
+set (ParserSrc 
+${CMAKE_CURRENT_LIST_DIR}/loglineinfo.cpp
+${CMAKE_CURRENT_LIST_DIR}/loglineinfo.h
+${CMAKE_CURRENT_LIST_DIR}/logparser.cpp
+${CMAKE_CURRENT_LIST_DIR}/logparser.h
+)
