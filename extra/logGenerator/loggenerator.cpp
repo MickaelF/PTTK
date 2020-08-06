bad08f0dfd5bb3f95e07196d71b9fd9d64c6d717
Mickaël Fleurus <fleurusmickael@gmail.com>
2020-08-06 21:02:34 +0200

    Remove priority combobox class and add back buttons for sorting.

--- extra/logGenerator/loggenerator.cpp
+++ extra/logGenerator/loggenerator.cpp
@@ -8,17 +8,24 @@
 
 LogGenerator::LogGenerator(const std::filesystem::path& outputPath, int nbLines,
                            std::optional<std::time_t> time)
-    :
-      m_outPath(outputPath),m_nbLines(nbLines), m_time(time), m_logger(outputPath)
+    : m_outPath(outputPath),
+      m_nbLines(nbLines),
+      m_time(time),
+      m_logger(outputPath)
 {
 }
 
 void LogGenerator::exec()
 {
+    using namespace LogPriority;
     const std::vector<std::string> randomFileName {"file1", "file2", "file3",
                                                    "file4", "file5", "file6"};
     const std::vector<std::string> logText {
-        "This is a log on one line.","This is a log on one line too, but this one is pretty long. Since every other log are reaaaally shot, this seems like a good idea to have at least one long one, to see what it looks like.", "This is \na log on two lines", "This\nis\ntoo\nmany\nlines"};
+        "This is a log on one line.",
+        "This is a log on one line too, but this one is pretty long. Since every other log are "
+        "reaaaally shot, this seems like a good idea to have at least one long one, to see what it "
+        "looks like.",
+        "This is \na log on two lines", "This\nis\ntoo\nmany\nlines"};
 
     std::time_t now = (m_time.has_value())
                           ? *m_time
@@ -26,8 +33,8 @@ void LogGenerator::exec()
     Log<false, true>::setLogger(m_logger);
     for (; m_progress < m_nbLines; ++m_progress)
     {
-        LogPriority priority = static_cast<LogPriority>(NumberGenerator::generateBetween(
-            static_cast<int>(LogPriority::Debug), static_cast<int>(LogPriority::Execution)));
+        Priorities priority =
+            static_cast<Priorities>(NumberGenerator::generateBetween(0, enumMaxNumber()));
         int fileId =
             NumberGenerator::generateBetween(0, static_cast<int>(randomFileName.size() - 1));
         int logTextId = NumberGenerator::generateBetween(0, static_cast<int>(logText.size() - 1));
