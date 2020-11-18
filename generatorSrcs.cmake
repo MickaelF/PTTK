edabe0fb39b991a8fdd68ca33c93cb0a09325e88
Mickael Fleurus <m.fleurus@oodrive.com>
2020-11-18 20:29:24 +0100

    Adding configuration files for cmake usage.

--- generatorSrcs.cmake
+++
@@ -1,7 +0,0 @@
-set (GeneratorSrcs 
-${CMAKE_CURRENT_LIST_DIR}/extra/logGenerator/loggenerator.cpp
-${CMAKE_CURRENT_LIST_DIR}/extra/logGenerator/loggenerator.h
-)
-set (GeneratorIncludeDir
-	${CMAKE_CURRENT_LIST_DIR}/extra/logGenerator/
-)
