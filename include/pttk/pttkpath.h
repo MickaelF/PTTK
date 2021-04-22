72906fc4a0a813b6ecfc70f0dbe82608fcb2bf89
Mickael Fleurus <m.fleurus@oodrive.com>
2021-04-22 14:02:35 +0200

    Adding document folder for windows in pttkPath

--- include/pttk/pttkpath.h
+++ include/pttk/pttkpath.h
@@ -2,8 +2,10 @@
 #include <filesystem>
 #include <string_view>
 
-
 namespace pttkPath
 {
-std::filesystem::path getDataPath(std::string_view executableName, std::string_view companyName);
-}
+std::filesystem::path getDataPath(std::string_view executableName,
+                                  std::string_view companyName);
+std::filesystem::path getDocumentFolderPath(std::string_view executableName,
+                                            std::string_view companyName);
+} // namespace pttkPath
--- src/pttkpath.cpp
+++ src/pttkpath.cpp
@@ -3,17 +3,38 @@
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
     #include <ShlObj.h>
 #endif
+
 namespace pttkPath
 {
-std::filesystem::path getDataPath(std::string_view executableName, std::string_view companyName)
+std::filesystem::path getDataPath(std::string_view executableName,
+                                  std::string_view companyName)
 {
     std::filesystem::path programDataPath;
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
     PWSTR path {NULL};
-    HRESULT hr = SHGetKnownFolderPath(FOLDERID_ProgramData, KF_FLAG_DEFAULT, NULL, &path);
+    HRESULT hr = SHGetKnownFolderPath(FOLDERID_ProgramData, KF_FLAG_DEFAULT,
+                                      NULL, &path);
     if (SUCCEEDED(hr)) programDataPath = path;
 #endif
-    programDataPath.append(std::string(companyName) + "/" + std::string(executableName));
+    programDataPath.append(std::string(companyName) + "/" +
+                           std::string(executableName));
+    if (!std::filesystem::exists(programDataPath))
+        std::filesystem::create_directories(programDataPath);
+    return programDataPath;
+}
+
+std::filesystem::path getDocumentFolderPath(std::string_view executableName,
+                                            std::string_view companyName)
+{
+    std::filesystem::path programDataPath;
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
+    PWSTR path {NULL};
+    HRESULT hr =
+        SHGetKnownFolderPath(FOLDERID_Documents, KF_FLAG_DEFAULT, NULL, &path);
+    if (SUCCEEDED(hr)) programDataPath = path;
+#endif
+    programDataPath.append(std::string(companyName) + "/" +
+                           std::string(executableName));
     if (!std::filesystem::exists(programDataPath))
         std::filesystem::create_directories(programDataPath);
     return programDataPath;
