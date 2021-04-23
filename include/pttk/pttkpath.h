1856dd60e2a6899a99d33001e417fb50e28862b2
Mickael Fleurus <m.fleurus@oodrive.com>
2021-04-23 16:10:06 +0200

    Change how the path system works.

--- include/pttk/pttkpath.h
+++ include/pttk/pttkpath.h
@@ -4,8 +4,12 @@
 
 namespace pttkPath
 {
-std::filesystem::path getDataPath(std::string_view executableName,
-                                  std::string_view companyName);
-std::filesystem::path getDocumentFolderPath(std::string_view executableName,
-                                            std::string_view companyName);
+    std::wstring getDataPathStr(std::string_view executableName,
+                                std::string_view companyName);
+    std::filesystem::path getDataPath(std::string_view executableName,
+                                      std::string_view companyName);
+    std::wstring getDocumentFolderPathStr(std::string_view executableName,
+                                          std::string_view companyName);
+    std::filesystem::path getDocumentFolderPath(std::string_view executableName,
+                                                std::string_view companyName);
 } // namespace pttkPath
