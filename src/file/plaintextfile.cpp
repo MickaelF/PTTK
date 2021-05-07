7755052c7d52ab321c9daa59e6380c146aaefd29
Mickael Fleurus <m.fleurus@oodrive.com>
2021-05-07 17:06:44 +0200

    Correct small mistake in plain text file reader.

--- src/file/plaintextfile.cpp
+++ src/file/plaintextfile.cpp
@@ -4,10 +4,12 @@
 
 std::optional<std::string> PlainTextFile::wholeFile(std::string_view str)
 {
-    if (!openToRead(str)) return std::nullopt;
+    if (!openToRead(str))
+        return std::nullopt;
     std::string file;
     std::string line;
-    while (std::getline(m_fileStream, line)) file += line;
+    while (std::getline(m_fileStream, line))
+        file += line + '\n';
     close();
     return file;
 }
